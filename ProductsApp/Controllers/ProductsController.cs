ec2cf65a806de2c050fed426d51593aa2417d237
KevinB6766 <tuu21653@temple.edu>
2026-04-14 16:55:15 -0400

    Changed product variables except for id

--- ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/Controllers/ProductsController.cs
@@ -12,9 +12,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Schmoceries", Price = 42 },
+            new Product { Id = 2, Name = "No-no", Category = "Joys", Price = 1.23M },
+            new Product { Id = 3, Name = "Slammer", Category = "Software", Price = 13.37M }
         };
         public ProductsController()
         {
